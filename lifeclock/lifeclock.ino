fef812f7c31f647702ed5591c3b1e5c8ef55fdf1
Jia Xun <chaijiaxun@gmail.com>
2019-08-16 00:46:43 -0700

    Button clicky

--- lifeclock/lifeclock.ino
+++ lifeclock/lifeclock.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+int LED_PIN = 6;
+int BTN_UP_PIN = 7;
+int BTN_DOWN_PIN = 5;
+int BTN_RESET_PIN = 4;
 
+// Working variables
+
+// Button states for UP, DOWN, RESET
+bool buttonStates [3] = {false, false, false};
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(LED_PIN, OUTPUT);
+  // initialize the pushbutton pins as an input:
+  pinMode(BTN_UP_PIN, INPUT);
+  pinMode(BTN_DOWN_PIN, INPUT);
+  pinMode(BTN_RESET_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonStates[0] = digitalRead(BTN_UP_PIN);
+  buttonStates[1] = digitalRead(BTN_DOWN_PIN);
+  buttonStates[2] = digitalRead(BTN_RESET_PIN);
 
+  if ( buttonStates[0] || buttonStates[1] || buttonStates[2] ) {
+    digitalWrite(LED_PIN, HIGH); 
+  } else {
+    digitalWrite(LED_PIN, LOW); 
+  }
 }
