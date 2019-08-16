0fe555b873aceadcfd20e92d9258477cbc92d462
Jia Xun <chaijiaxun@gmail.com>
2019-08-16 00:53:33 -0700

    Button counter

--- lifeclock/lifeclock.ino
+++ lifeclock/lifeclock.ino
@@ -6,8 +6,10 @@ int BTN_RESET_PIN = 4;
 // Working variables
 
 // Button states for UP, DOWN, RESET
+bool buttonStatesPrev [3] = {false, false, false};
 bool buttonStates [3] = {false, false, false};
 
+int counter = 0;
 void setup() {
   // initialize the LED pin as an output:
   pinMode(LED_PIN, OUTPUT);
@@ -15,16 +17,45 @@ void setup() {
   pinMode(BTN_UP_PIN, INPUT);
   pinMode(BTN_DOWN_PIN, INPUT);
   pinMode(BTN_RESET_PIN, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  digitalWrite(LED_PIN, LOW); 
   buttonStates[0] = digitalRead(BTN_UP_PIN);
   buttonStates[1] = digitalRead(BTN_DOWN_PIN);
   buttonStates[2] = digitalRead(BTN_RESET_PIN);
 
-  if ( buttonStates[0] || buttonStates[1] || buttonStates[2] ) {
-    digitalWrite(LED_PIN, HIGH); 
-  } else {
-    digitalWrite(LED_PIN, LOW); 
+
+  // Detect if a button is clicked (state change from low to high)
+  if ( buttonStates[0] && !buttonStatesPrev[0]) {
+    digitalWrite(LED_PIN, HIGH);
+    counter ++;
+      Serial.print(counter);
+  Serial.print("\n");
   }
+
+  if ( buttonStates[1] && !buttonStatesPrev[1]) {
+    digitalWrite(LED_PIN, HIGH); 
+
+    counter --;
+      Serial.print(counter);
+  Serial.print("\n");
+  }
+
+  if ( buttonStates[2] && !buttonStatesPrev[2]) {
+    digitalWrite(LED_PIN, HIGH); 
+
+    counter = 0;
+      Serial.print(counter);
+  Serial.print("\n");
+  }
+
+  for ( int i = 0; i < 3; i++ ) {
+    buttonStatesPrev[i] = buttonStates[i];
+  }
+
+
+
+  delay(10);
 }
